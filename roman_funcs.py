bb2a0693a0b8f5c49015bc01fc1fd2246e85980d
Martin Rivas <martin.rivas.r@hotmail.com>
2024-07-02 00:44:56 -0600

    addicion de decenas, centenas y millar

--- roman_funcs.py
+++ roman_funcs.py
@@ -1,17 +1,55 @@
-def to_roman(valor):
-    result = ""
-    if valor == 1:
-        result = "I"
-    elif valor == 5:
-        result ="V"
-    elif valor == 10:
-        result = "X"
-    elif valor == 50:
-        result = "L"
-    elif valor == 100:
-        result = "C"
-    elif valor == 500:
-        result = "D"
-    elif valor == 1000:
-        result = "M"
+valores = {
+    1: 'I',
+    5: 'V',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M',
+
+}
+
+
+def to_roman(n):
+    result = ''
+
+    #millares
+    if n >= 1000:
+        result = (n//1000) * valores[1000]
+        n -= (n // 1000) * 1000
+
+    if n >= 100:
+        if n >= 900:
+             result += valores[100] + valores[1000]
+        elif n >= 500:
+            result += valores[500] + ((n // 100)  - 5 )* valores[100]
+        elif n == 400:
+            result += valores[100] + valores[500]
+        else:
+            result += (n // 100) * valores[100]
+        n -= (n // 100) * 100
+    
+    if n >= 10:
+        if n >= 90:
+            result += valores[10] + valores[100]
+        elif n >= 50:
+            result += valores[50] + ((n//10)-5) * valores[10]
+        elif n == 40:
+            result += valores[10] + valores[50]
+        else:
+            result += (n//10) * valores[10]
+        n -= (n // 10 ) * 10
+    
+    if n >= 1:
+        if n == 9:
+            result += valores[1] + valores[10]
+        elif n >= 5:
+            result += valores[5] + (n - 5) * valores[1]
+        elif n == 4:
+            result += valores[1] + valores[5]
+        else:
+            result += n * valores[1]
+        
     return result
+
+
--- test_roman_number.py
+++ test_roman_number.py
@@ -1,11 +1,40 @@
 from roman_funcs import *
 
+print(to_roman(10))
 def test_romanos_simples():
-    assert to_roman(1) == "I"
-    assert to_roman(5) == "V"
-    assert to_roman(10) == "X"
-    assert to_roman(50) == "L"
-    assert to_roman(100) == "C"
-    assert to_roman(500) == "D"
-    assert to_roman(1000) == "M"
-    
+    assert to_roman(1) == 'I'
+    assert to_roman(2) == 'II'
+    assert to_roman(3) == 'III'
+    assert to_roman(4) == 'IV'
+    assert to_roman(5) == 'V'
+    assert to_roman(6) == 'VI'
+    assert to_roman(7) == 'VII'
+    assert to_roman(8) == 'VIII'
+    assert to_roman(9) == 'IX'
+    assert to_roman(10) == 'X' 
+    assert to_roman(20) == 'XX'
+    assert to_roman(30) == 'XXX'
+    assert to_roman(40) == 'XL'
+    assert to_roman(50) == 'L'
+    assert to_roman(60) == 'LX'
+    assert to_roman(70) == 'LXX'
+    assert to_roman(80) == 'LXXX'
+    assert to_roman(90) == 'XC' 
+
+    assert to_roman(100) == 'C' 
+    assert to_roman(200) == 'CC'
+    assert to_roman(300) == 'CCC'
+    assert to_roman(400) == 'CD'
+    assert to_roman(500) == 'D'
+    assert to_roman(600) == 'DC'
+    assert to_roman(700) == 'DCC'
+    assert to_roman(800) == 'DCCC'
+    assert to_roman(900) == 'CM' 
+
+    assert to_roman(58) == 'LVIII' 
+    assert to_roman(1994) == 'MCMXCIV' 
+    assert to_roman(3999) == 'MMMCMXCIX' 
+    assert to_roman(723) == 'DCCXXIII' 
+    assert to_roman(79) == 'LXXIX' 
+    assert to_roman(999) == 'CMXCIX'
+
